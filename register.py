34a281a6a74d577a81cc7dd3ef086591c9414e7d
pardhusa <pardhusince2004@gmail.com>
2026-03-08 15:31:35 +0530

    Nothing

--- register.py
+++ register.py
@@ -14,6 +14,7 @@ import os
 from deepface import DeepFace
 
 # ================= CONFIG =================
+
 SAMPLES_REQUIRED = 10
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 # =========================================
